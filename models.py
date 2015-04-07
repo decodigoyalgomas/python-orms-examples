c453deb211939eeabc46b08b2d45960c9b64af5e
ondoheer <ondoheer@gmail.com>
2015-04-07 11:29:36 -0500

    modificando la estructura de sqalchemy para entender a fondo las relaciones

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 
 #in memory only sqlite db
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine('sqlite:///test.db', echo=True)
 
 # Lazy Connecting
 # The Engine, when first returned by create_engine(), has not actually tried to connect to the database yet; that happens only the first time it is asked to perform a task against the database.
@@ -21,9 +21,9 @@ class User(Base):
     password = Column(String(50))
     email = Column(String(50))
 
-    posts = relationship("Post", order_by="Post.id", backref="user")
-    comments = relationship("Comment", order_by="Comment.id", backref="user")
-    replies = relationship("Reply", order_by="Reply.id", backref="user")
+    posts = relationship("Post", order_by="Post.id", backref="usr")
+    # comments = relationship("Comment", order_by="Comment.id", backref="user")
+    # replies = relationship("Reply", order_by="Reply.id", backref="user")
 
     def __repr__(self):
         return '<User {}, with email: {}>'.format(self.nickname, self.email)
@@ -40,7 +40,7 @@ class Post(Base):
 
     user = relationship("User", backref=backref('users', order_by=id))
 
-    comments = relationship("Comment", order_by="Comment.id", backref="post")
+    #comments = relationship("Comment", order_by="Comment.id", backref="post")
     
     
 
@@ -48,40 +48,49 @@ class Post(Base):
         return '<Post {}, with title "{}">'.format(self.id, self.title)
 
 
-class Comment(Base):
+# class Comment(Base):
 
-    __tablename__ = 'comments'
+#     __tablename__ = 'comments'
 
-    id = Column(Integer, primary_key=True)
+#     id = Column(Integer, primary_key=True)
 
-    title = Column(String(255))
-    body = Column(Text)
-    user_id = Column(Integer, ForeignKey('users.id'))  
-    post_id = Column(Integer, ForeignKey('posts.id'))
+#     title = Column(String(255))
+#     body = Column(Text)
+#     user_id = Column(Integer, ForeignKey('users.id'))  
+#     post_id = Column(Integer, ForeignKey('posts.id'))
 
-    user = relationship("User", backref=backref('comments', order_by=id))
-    post = relationship("Post", backref=backref('comments', order_by=id))    
+#     user = relationship("User", backref=backref('comments', order_by=id))
+#     post = relationship("Post", backref=backref('comments', order_by=id))    
 
-    replies = relationship("Reply", order_by="Reply.id", backref="comment")
+#     replies = relationship("Reply", order_by="Reply.id", backref="comment")
 
-    def __repr__(self):
-        return '<Comment {}, with title "{}">'.format(self.id, self.title)
+#     def __repr__(self):
+#         return '<Comment {}, with title "{}">'.format(self.id, self.title)
 
 
-class Reply(Base):
+# class Reply(Base):
 
-    __tablename__ = 'replies'
+#     __tablename__ = 'replies'
 
-    id = Column(Integer, primary_key=True)
+#     id = Column(Integer, primary_key=True)
 
-    body = Column(Text)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    comment_id = Column(Integer, ForeignKey('comments.id'))
+#     body = Column(Text)
+#     user_id = Column(Integer, ForeignKey('users.id'))
+#     comment_id = Column(Integer, ForeignKey('comments.id'))
 
   
-    user = relationship("User", backref=backref('replies', order_by=id))
-    comment = relationship("Comment", backref=backref('replies', order_by=id))
+#     user = relationship("User", backref=backref('replies', order_by=id))
+#     comment = relationship("Comment", backref=backref('replies', order_by=id))
 
-    def __repr__(self):
-        return 'Reply {}, to comment {}'.format(self.id, self.comment)
+#     def __repr__(self):
+#         return 'Reply {}, to comment {}'.format(self.id, self.comment)
 
+
+# Create all tables in the engine. This is equivalent to "Create Table"
+# statements in raw SQL.
+Base.metadata.create_all(engine)
+
+from sqlalchemy.orm import sessionmaker
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
